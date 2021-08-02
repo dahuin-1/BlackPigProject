9d5c93d0afc3bdc76259bf7fd4ab78f6c8d91db6
dahuin-1 <dawhite01@naver.com>
2021-08-02 18:28:45 +0900

    update

--- src/main/java/com/huineey/blackpigproject/controller/BoardController.java
+++ src/main/java/com/huineey/blackpigproject/controller/BoardController.java
@@ -1,15 +1,29 @@
 package com.huineey.blackpigproject.controller;
 
+import com.huineey.blackpigproject.model.Board;
+import com.huineey.blackpigproject.repository.BoardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.flyway.FlywayDataSource;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/board")
 public class BoardController {
 
+    @Autowired
+    private BoardRepository boardRepository;
+
     @GetMapping("list")
-    public String list() {
+    public String list(Model model) {
+        List<Board> boards = boardRepository.findAll();
+        model.addAttribute("boards", boards);
         return "board/list";
     }
 
