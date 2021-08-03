6673efa8faa656b8dcc91bf9618347f0e1fa7f29
jongseon.woo <jongseon.woo@navercorp.com>
2021-08-03 18:36:12 +0900

    pass boardlist to index.html

--- src/main/java/com/huineey/blackpigproject/controller/HomeController.java
+++ src/main/java/com/huineey/blackpigproject/controller/HomeController.java
@@ -1,14 +1,26 @@
 package com.huineey.blackpigproject.controller;
 
+import com.huineey.blackpigproject.model.Board;
+import com.huineey.blackpigproject.repository.BoardRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
 
-    @GetMapping
-    public String index() {
+    private BoardRepository boardRepository;
 
+    public HomeController(BoardRepository boardRepository){
+        this.boardRepository = boardRepository;
+    }
+
+    @GetMapping
+    public String index(Model model) {
+        List<Board> boardList = boardRepository.findAll();
+        model.addAttribute("boards", boardList);
         //디비에 식당 저장해두고
         //그걸 불러와서
         //에드어트리뮤트
