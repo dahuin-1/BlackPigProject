5b17d264aa7458063dbe086604c1f823ff86e8af
dahuin-1 <dawhite01@naver.com>
2021-09-19 23:37:43 +0900

    add store link

--- src/main/java/com/huineey/blackpigproject/controller/StoreApiController.java
+++ src/main/java/com/huineey/blackpigproject/controller/StoreApiController.java
@@ -4,10 +4,10 @@ import com.huineey.blackpigproject.model.Store;
 import com.huineey.blackpigproject.repository.BoardRepository;
 import com.huineey.blackpigproject.repository.StoreRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.thymeleaf.util.StringUtils;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -17,9 +17,19 @@ class StoreApiController {
     private StoreRepository storeRepository;
 
     @GetMapping("/stores/{id}")
-    Store one(@PathVariable Long id) {
-        return storeRepository.findById(id).orElse(null);
+    Store one(@RequestParam(required = false, defaultValue = "") String storeId) {
+        return storeRepository.findById(storeId);
     }
+
+   /* @GetMapping("/boards")
+    List<Board> all(@RequestParam(required = false, defaultValue = "") String title,
+                    @RequestParam(required = false, defaultValue = "") String content) {
+        if(StringUtils.isEmpty(title) && StringUtils.isEmpty(content)) {
+            return repository.findAll();
+        }
+        return repository.findByTitleOrContent(title, content);
+    }*/
+
 
 
 }
--- src/main/java/com/huineey/blackpigproject/repository/StoreRepository.java
+++ src/main/java/com/huineey/blackpigproject/repository/StoreRepository.java
@@ -4,9 +4,11 @@ import com.huineey.blackpigproject.model.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 
 public interface StoreRepository extends JpaRepository<Store, Long> {
     List<Store> findAll();
+    Store findById(String storeId);
 }
 
