d7ec54c8da2f9b705a13ae2af2d1f846ab619f16
dahuin-1 <dawhite01@naver.com>
2021-10-24 02:03:54 +0900

    add comment

--- src/main/java/com/huineey/blackpigproject/repository/UserRepository.java
+++ src/main/java/com/huineey/blackpigproject/repository/UserRepository.java
@@ -12,3 +12,9 @@ public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
 }
 // reset aad51cef075912fafd4eb3a17fe7bb067e5cc8d7
+
+// 리셋하고 싶은 커밋 id AAAA , master 브랜치 , nbranch
+// master 에서 git checkout -b <임시저장할 브랜치 이름>
+// AAAA 내용으로 master를 리셋하기  git reset --hard AAAA
+// 리셋한 master에서 아무 내용이나 추가해서 커밋하기
+// git push -f origin master로 푸시하기
