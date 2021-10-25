cfd5a6e71403ba6eebd3d715f3efb2dc510af958
dahuin-1 <dawhite01@naver.com>
2021-10-25 21:53:47 +0900

    add social login

--- src/main/java/com/huineey/blackpigproject/controller/GoogleController.java
+++ src/main/java/com/huineey/blackpigproject/controller/GoogleController.java
@@ -18,6 +18,7 @@ import org.springframework.web.client.RestTemplate;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.Map;
 
 @Controller
@@ -26,13 +27,13 @@ public class GoogleController {
     @Autowired
     GoogleConnectionFactory googleConnectionFactory;
     @Autowired
-    OAuth2Parameters googleoAuth2Parameters;
+    OAuth2Parameters googleOAuth2Parameters;
 
-    // 구글 로그인 페이지를 보여준다.
+    // 구글 로그인 페이지
     @GetMapping("/personal")
     public String main() {
         OAuth2Operations oAuth2Operations = googleConnectionFactory.getOAuthOperations();
-        String url = oAuth2Operations.buildAuthenticateUrl(GrantType.AUTHORIZATION_CODE,googleoAuth2Parameters);
+        String url = oAuth2Operations.buildAuthenticateUrl(GrantType.AUTHORIZATION_CODE,googleOAuth2Parameters);
         System.out.println(url);
         return "redirect:"+url;
     }
@@ -40,13 +41,12 @@ public class GoogleController {
     @GetMapping("/google/callback")
     public String GetRequestToken(HttpServletRequest request) throws JsonParseException, JsonMappingException, IOException {
 
-        //RestTemplate을 사용하여 Access Token 및 profile을 요청한다.
         RestTemplate restTemplate = new RestTemplate();
         MultiValueMap<String, String> parameters = new LinkedMultiValueMap<>();
         parameters.add("code", request.getParameter("code"));
         parameters.add("client_id", "1005769212578-sagsuujgoisaih1j0c4vrlt3la6dj564.apps.googleusercontent.com");
         parameters.add("client_secret", "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");
-        parameters.add("redirect_uri", googleoAuth2Parameters.getRedirectUri());
+        parameters.add("redirect_uri", googleOAuth2Parameters.getRedirectUri());
         parameters.add("grant_type", "authorization_code");
 
         HttpHeaders headers = new HttpHeaders();
@@ -57,18 +57,17 @@ public class GoogleController {
 
         System.out.println(responseEntity.toString());
 
-
         String[] tokens = ((String)responseMap.get("id_token")).split("\\.");
         Base64 base64 = new Base64(true);
         String body = new String(base64.decode(tokens[1]));
 
         System.out.println(tokens.length);
-        System.out.println(new String(Base64.decodeBase64(tokens[0]), "utf-8"));
-        System.out.println(new String(Base64.decodeBase64(tokens[1]), "utf-8"));
+        System.out.println(new String(Base64.decodeBase64(tokens[0]), StandardCharsets.UTF_8));
+        System.out.println(new String(Base64.decodeBase64(tokens[1]), StandardCharsets.UTF_8));
 
-        //Jackson을 사용한 JSON을 자바 Map 형식으로 변환
+        //JSON을 자바 Map 형식으로 변환
         ObjectMapper mapper = new ObjectMapper();
-        Map<String, String> result = mapper.readValue(body, Map.class);
+        Map result = mapper.readValue(body, Map.class);
         System.out.println(result.get(""));
 
 
