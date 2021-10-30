8899966d52e225b90cffda7533fbdf282ee29e62
dahuin-1 <dawhite01@naver.com>
2021-10-30 16:40:52 +0900

    change gallery.html loc to board folder

--- src/main/java/com/huineey/blackpigproject/controller/PictureController.java
+++ src/main/java/com/huineey/blackpigproject/controller/PictureController.java
@@ -5,9 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/board")
 public class PictureController {
     @GetMapping("/gallery")
     public String show() {
-        return "/gallery";
+        return "board/gallery";
     }
 }
