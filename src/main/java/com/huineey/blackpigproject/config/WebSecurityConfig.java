052606f0ab2dfa478d6592d8ad55e86e9e407987
dahuin-1 <dawhite01@naver.com>
2021-11-01 15:28:42 +0900

    edit css loc.

--- src/main/java/com/huineey/blackpigproject/config/WebSecurityConfig.java
+++ src/main/java/com/huineey/blackpigproject/config/WebSecurityConfig.java
@@ -37,7 +37,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/", "/account/**", "/css/**", "/static/css/**","/api/**","/board/list","/personal/**").permitAll()
+                .antMatchers("/", "/account/**","/css/**","/api/**","/board/**","/personal/**","/gallery").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 /*.addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider),
