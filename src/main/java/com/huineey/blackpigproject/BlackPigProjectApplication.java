29b4f62a56fd89864c13b66a1bbdd56f5b97f0d5
dahuin-1 <dawhite01@naver.com>
2021-11-14 02:46:45 +0900

    edit pom.xml

--- src/main/java/com/huineey/blackpigproject/BlackPigProjectApplication.java
+++ src/main/java/com/huineey/blackpigproject/BlackPigProjectApplication.java
@@ -2,7 +2,7 @@ package com.huineey.blackpigproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+
 
 @SpringBootApplication
 public class BlackPigProjectApplication {
